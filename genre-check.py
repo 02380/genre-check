6a2d1b16fbf496ecb43ecb150a617ec726f7236f
02380 <49149558+02380@users.noreply.github.com>
2020-06-15 18:55:41 +0100

    ready to test deploy

--- genre-check.py
+++ genre-check.py
@@ -2,13 +2,16 @@
 
 import argparse
 import os
+import time
+import math
 from pprint import pprint
 import eyed3
 
+eyed3.log.setLevel("ERROR")
 parser = argparse.ArgumentParser()
 parser.add_argument("cwd")
 args = parser.parse_args()
-print(args.cwd)
+print("genre-checker: scanning {0}".format(args.cwd))
 
 mp3s = []
 songs = []
@@ -27,5 +30,6 @@ for song in songs:
 	if hasattr(audiofile, 'tag'):
 		if hasattr(audiofile.tag, 'genre'):
 			songs_ok.append(song)
+	time.sleep(0.01)
 
-print("{0} songs found - {1} are tagged, {2} are not [{3}%]".format(len(songs),len(songs_ok),len(songs)-len(songs_ok),len(songs_ok)/len(songs)))
+print("{0} songs found - {1} are tagged, {2} are not [{3}%]".format(len(songs),len(songs_ok),len(songs)-len(songs_ok),round((len(songs_ok)/len(songs))*100,2)))
