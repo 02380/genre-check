eb77287c758be731d95ad675122cbd8a014f33e6
02380 <49149558+02380@users.noreply.github.com>
2020-06-15 19:26:51 +0100

    tinytag is way better

--- genre-check.py
+++ genre-check.py
@@ -1,5 +1,6 @@
 #!venv/bin/python
 
+from tinytag import TinyTag
 import argparse
 import os
 import time
@@ -7,9 +8,10 @@ import math
 from pprint import pprint
 import eyed3
 
+no_info_doc = "LOG\n\n"
+
 def log_bad_song(s):
-	with open('log.txt', 'a') as the_file:
-	    the_file.write('NO INFO: {0}\n'.format(s))
+	return str('NO INFO: {0}\n'.format(s))
 
 eyed3.log.setLevel("ERROR")
 parser = argparse.ArgumentParser()
@@ -31,14 +33,26 @@ for root, dirs, files in os.walk(args.cwd):
         	songs.append(os.path.join(root, filename))
 
 for song in songs:
-	audiofile = eyed3.load(song)
-	if hasattr(audiofile, 'tag'):
-		if hasattr(audiofile.tag, 'genre'):
+	audiofile = TinyTag.get(song)
+	# audiofile = eyed3.load(song)
+	# if hasattr(audiofile, 'tag'):
+	# 	if hasattr(audiofile.tag, 'genre'):
+	# 		songs_ok.append(song)
+	# 	else:
+	# 		log_bad_song(song)
+	# else:
+	# 	log_bad_song(song)
+	log_bad_song(audiofile.genre)
+	if hasattr(audiofile, 'genre'):
+		if audiofile.genre == 'None' or audiofile.genre == None or audiofile.genre == '':
+			no_info_doc += log_bad_song(song)
+		else:	
 			songs_ok.append(song)
-		else:
-			log_bad_song(song)
 	else:
 		log_bad_song(song)
 	time.sleep(0.01)
 
+
+with open('log.txt', 'w') as the_file:
+    the_file.write(no_info_doc)
 print("{0} songs found - {1} are tagged, {2} are not [{3}%]".format(len(songs),len(songs_ok),len(songs)-len(songs_ok),round((len(songs_ok)/len(songs))*100,2)))
