f518c331891282bb957670531a0a67a2684090b4
02380 <49149558+02380@users.noreply.github.com>
2020-06-15 18:50:39 +0100

    clear out crap

--- genre-check.py
+++ genre-check.py
@@ -22,15 +22,10 @@ for root, dirs, files in os.walk(args.cwd):
         elif os.path.splitext(filename)[1] == ".m4a":
         	songs.append(os.path.join(root, filename))
 
-# print(songs[110])
-# audiofile = eyed3.load(songs[110])
-# print(audiofile.tag.genre)
-
 for song in songs:
 	audiofile = eyed3.load(song)
 	if hasattr(audiofile, 'tag'):
 		if hasattr(audiofile.tag, 'genre'):
-			print("song good!")
 			songs_ok.append(song)
 
 print("{0} songs found - {1} are tagged, {2} are not [{3}%]".format(len(songs),len(songs_ok),len(songs)-len(songs_ok),len(songs_ok)/len(songs)))
